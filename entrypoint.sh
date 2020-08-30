f634b5155340a249a568ff570992f3f04dcb4108
Дахновский Дмитрий Александрович <dmitriy.dakhnovskiy@south.rt.ru>
2020-08-30 21:29:02 +0300

    add alembic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python scripts/make_migrations.py
+
 if [[ ${ENVIRONMENT} = 'LOCAL' ]]; then
     exec uvicorn src.app.app:app --reload --host 0.0.0.0 --port 5000 --log-level info
 else
---
+++ scripts/make_migrations.py
@@ -0,0 +1,14 @@
+from src.app.config import config
+import alembic.config
+import alembic.command
+
+
+def make_migrations():
+    alembic_cfg = alembic.config.Config()
+    alembic_cfg.set_main_option('script_location', 'alembic')
+    alembic_cfg.set_main_option('sqlalchemy.url', config.PG_DSN)
+    alembic.command.upgrade(alembic_cfg, 'head')
+
+
+if __name__ == '__main__':
+    make_migrations()
