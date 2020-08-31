697dd839e99cb8aac4fc765750b431f99321c8e3
Дахновский Дмитрий Александрович <dmitriy.dakhnovskiy@south.rt.ru>
2020-08-31 11:46:14 +0300

    add force_rollback parameter for testing environment

--- src/app/db.py
+++ src/app/db.py
@@ -3,5 +3,5 @@ import sqlalchemy
 
 from src.app.config import config
 
-db = Database(config.PG_DSN)
+db = Database(config.PG_DSN, force_rollback=(config.ENVIRONMENT == 'TESTING'))
 metadata = sqlalchemy.MetaData()
